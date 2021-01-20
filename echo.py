3ea24b345dcdb7e52c33c687e2cf32b437bf9532
akhilj210f <akhilchandhuj210f@gmail.com>
2021-01-20 14:42:06 +0530

    Echo function

---
+++ echo.py
@@ -0,0 +1,10 @@
+##THis is an example prgm to use echo command in python to print something...
+## echo is a commandused to print some thing in python
+##syntax:
+##echo "<Anything which you want to display>";
+
+##EG:
+echo "This program print this sentance";
+
+
+##TO run python echo.py
