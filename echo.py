e8f0b82e9e25eb555f0b2d70568c8a40f172f94b
akhilj210f <akhilchandhuj210f@gmail.com>
2021-01-20 14:57:40 +0530

    LOL added php echo in python

--- echo.py
+++ echo.py
@@ -1,10 +1,11 @@
-##THis is an example prgm to use echo command in python to print something...
-## echo is a commandused to print some thing in python
+##THis is an example prgm to use print() command in python to print something...
+## a command used to print some thing in python
 ##syntax:
-##echo "<Anything which you want to display>";
+##print("<Anything which you want to display>")
 
 ##EG:
-echo "This program print this sentance";
+
+print("This program print this sentance")
 
 
 ##TO run python echo.py
