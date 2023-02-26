e66f9f51f80ba0f8f12195bb3df59d8f7d44f58f
Akhil R <akhil@YoUArEMIne.localdomain>
2023-02-26 15:24:33 +0530

    Added bug fixes

--- echo_bot.py
+++ echo_bot.py
@@ -1,11 +1,16 @@
-
-
-import logging,pickle,pytube
+import logging
+import pickle
 import os
 import shutil
+from telnetlib import STATUS
+import uuid
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from pytube import YouTube
+# from pytube import YouTube
+# import youtube_dl
+import yt_dlp
 import instaloader
+import requests
+import json
 """bot files """
 #from chatterbot import ChatBot
 #from chatterbot.trainers import ChatterBotCorpusTrainer
@@ -16,7 +21,7 @@ import instaloader
 #trainer = ChatterBotCorpusTrainer(chatbot)
 
 # Train the chatbot based on the english corpus
-#trainer.train("chatterbot.corpus.english")
+# trainer.train("chatterbot.corpus.english")
 # Get a response to an input statement
 
 # Enable logging
@@ -33,11 +38,14 @@ def start(update, context):
     update.message.reply_text('നമസ്കാരം!')
     update.message.reply_text('എന്ത് ഉണ്ട് വിശേഷം 😋')
 
+
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('എന്ത് സഹായം ആണ് വേണ്ടത്?')
     update.message.reply_text('@Akhil_R143 ചോദിക്ക് അവൻ പറഞ്ഞ് തരും')
-    update.message.reply_text('I only give replay if you mention me with my tag "@lostlover_bot" 😋')
+    update.message.reply_text(
+        'I only give replay if you mention me with my tag "@lostlover_bot" 😋')
+
 
 def who(update, context):
     update.message.reply_text('iam a super duper bot to bring to hell')
@@ -45,39 +53,56 @@ def who(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    print(update.message.chat.first_name,":",update.message.text)
+    print(update.message.chat.first_name, ":", update.message.text)
     """user input"""
-    user_input=update.message.text
+    user_input = update.message.text
     """BOT REPLY """
-    #bot_reply=user_input.replace("@lostlover_bot",'')
+    # bot_reply=user_input.replace("@lostlover_bot",'')
     #fullstring = update.message.text
     #substring = "@lostlover_bot"
-    #if substring in fullstring:
+    # if substring in fullstring:
     #    print("Found!")
-     #   update.message.reply_text(bot_reply)
-    #else:
-     #   print(" @lostlover_bot Not found!")
+    #   update.message.reply_text(bot_reply)
+    # else:
+    #   print(" @lostlover_bot Not found!")
     """update.message.reply_text(update.message.text)"""
     """update.message.replay_text()"""
-    
 
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+
 def spam(update, context):
     for i in range(3):
         update.message.reply_text("spamming....")
 
-def morning(update, context):
-    fullstring = update.message.text
-    substring = "morning" or "Morning" or "mrng" or "Mrng"
-    if substring in fullstring:
+
+def wish(update, context):
+    fullstring = str.upper(update.message.text)
+    strmorning = ["MORNING","MRNG"]
+    straftn = ["AFTERNOON","GOOD AFTERNOON"]
+    strgn = ["NIGHT","GN"]
+    strbye = "BYE"
+    brknstr = ["D0","BROKEN"]
+    heystr = ["HEY","HI","HAI"]
+    if fullstring in strmorning:
         print("said good morning")
-        update.message.reply_text("good morning")
-    else:
-        print(" nothing to deal with..")
+        update.message.reply_text("Good Morning Dear😊")
+    elif fullstring in straftn:
+        update.message.reply_text("Good Afternoon,had lunch?")
+    elif fullstring in strgn:
+        update.message.reply_text("Good Night")
+    elif fullstring in strbye:
+        update.message.reply_text("Bye👋 take care.")
+    elif fullstring in brknstr:
+        Broken(update,context)
+    elif fullstring in heystr:
+        update.message.reply_text("Hi👋,Wasup bro!")
+    #else:
+        #print("nothing to deal with.. Message:"+update.message.text)
+
 
 def chatai(update, context):
     answer = chatbot.get_response(update.message.text)
@@ -87,36 +112,80 @@ def chatai(update, context):
 
 ######################## The downloader ########################################
 
-def ytdl(update , context):
+def  ytdl(update, context):
     link = update.message.text.split()
     ####################VARIABLES#######################
-    try:yturl = link[1]
+    try:
+        yturl = link[1]
     except:
         update.message.reply_text("missed link? or wrong link?")
         update.message.reply_text("Format :  /ytdl <youtube  link> ")
-    save_path = "ytdl/"
+        return
     ####################################################
-    
-
-
-    update.message.reply_text("Currently under construction.... ")
-    update.message.reply_text("Sorry for the in conviniance occured.")
-
+    # where to save
+    SAVE_PATH = "downloads/"  # to_do
+    uuids = str(uuid.uuid4())
+    filename = uuids+".mp3"
+    #main code
+    ydl_opts = {
+        'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+    'outtmpl': os.path.join(SAVE_PATH, filename)
+    }
+    # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #     ydl.download([yturl])
+    update.message.reply_text("Downloading audio..")
+    try:
+        data = yt_dlp.YoutubeDL(ydl_opts).download([yturl])
+        update.message.reply_text("Uploading...")
+        filepath = 'downloads/'+filename
+        update.message.reply_document(open(filepath, 'rb'))
+        update.message.reply_text("Completed..")
+    except:
+        update.message.reply_text("Download failed.Sorry for the inconviniance occured.")
 
 #################################################################################
-
+def ytdlnew(update,context):
+    link = update.message.text.split()
+    ####################VARIABLES#######################
+    try:
+        yturl = link[1]
+    except:
+        update.message.reply_text("missed link? or wrong link?")
+        update.message.reply_text("Format :  /ytdl <youtube  link> ")
+    ####################################################
+    # where to save
+    SAVE_PATH = "downloads/"  # to_do
+    #main code
+    ydl_opts = {
+        'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+    'outtmpl': os.path.join(SAVE_PATH, '%(title)s-%(id)s.%(ext)s'),
+    'progress_hooks':[{update.message.reply_text('Downloading')}]
+    }
+    data = yt_dlp.YoutubeDL.download(yturl)
 
 ################################ Instagram DP downloader #################################################
 #Source : instaloader
 
-def instadp(update , context):
-    tmp= update.message.text.split()
+def instadp(update, context):
+    tmp = update.message.text.split()
     username = tmp[1]
-    update.message.reply_text("Downloading instagram profile picture of username: "+username)
-    ig = instaloader.Instaloader(dirname_pattern="dpfolder/",filename_pattern=None,title_pattern=username)
+    update.message.reply_text(
+        "Downloading instagram profile picture of username: "+username)
+    ig = instaloader.Instaloader(
+        dirname_pattern="dpfolder/", filename_pattern=None, title_pattern=username)
     dp = username
-    ig.download_profile(dp , profile_pic_only=True)
-    chatid=update.message.chat.id
+    ig.download_profile(dp, profile_pic_only=True)
+    chatid = update.message.chat.id
     directory_path = os.getcwd()
     filepath = directory_path+"/dpfolder/"+username+".jpg"
     update.message.reply_document(open(filepath, 'rb'))
@@ -125,17 +194,32 @@ def instadp(update , context):
     fromuser = update.message.chat.username
     print("Profile pic requested by "+fromuser+" is successfully uploaded..")
     update.message.reply_text("Done.")
-    
 
+#############################################################################
+# get random quotes
+
+
+def getQuotes(update, contex):
+    apiURL = "http://api.quotable.io/random"
+    r = requests.get(url=apiURL)
+    data = r.json()
+    update.message.reply_text("A Quote for you 😊: "+data['content'])
 
 
 #################################################################################
+##############################You broken####################################
+def Broken(update , context):
+    update.message.reply_text("You are really a peice of shit bro. shame 🗿")
+    update.message.reply_text("STF and do 20 pushups right now.")
+#################################################################################
+
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("", use_context=True)
+    updater = Updater(
+        "TOKEN HERE", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -147,10 +231,11 @@ def main():
     dp.add_handler(CommandHandler("spam", spam))
     dp.add_handler(CommandHandler("ytdl", ytdl))
     dp.add_handler(CommandHandler("instadp", instadp))
+    dp.add_handler(CommandHandler("quotes", getQuotes))
 
     # on noncommand i.e message - echo the message on Telegram
     """dp.add_handler(MessageHandler(Filters.text, echo))"""
-    dp.add_handler(MessageHandler(Filters.text, morning))
+    dp.add_handler(MessageHandler(Filters.text, wish))
     #dp.add_handler(MessageHandler(Filters.text, chatai))
 
     # log all errors
